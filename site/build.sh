a4df6a8604da0b64f09990d9472b34bf83aded3a
Chris Beaven <smileychris@gmail.com>
2026-04-07 08:25:48 +1200

    fix: update build.sh to use addventure command (was adv)

--- site/build.sh
+++ site/build.sh
@@ -3,6 +3,6 @@ set -e
 # Build docs, example PDF, then overlay the custom landing page
 cd "$(dirname "$0")/.."
 zensical build -c "$@"
-uv run adv build games/example -o site/assets/the-facility.pdf
+uv run addventure build games/example -o site/assets/the-facility.pdf
 cp site/index.html site/build/index.html
 cp -r site/assets site/build/
